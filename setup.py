7ba74e54ea180c1517adf4e6d0d6e1841069bf9b
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-08-17 14:14:24 +0100

    docs: Add repository links and format setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,23 @@
-import os
-from glob import glob
+from setuptools import find_packages, setup
 
-from setuptools import setup, find_packages
-
-PACKAGE_NAME = 'sentry_rclpy'
+PACKAGE_NAME = "sentry_rclpy"
 
 setup(
     name=PACKAGE_NAME,
-    version='0.1.0',
+    version="0.1.0",
     packages=find_packages(),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + PACKAGE_NAME]),
-        ('share/' + PACKAGE_NAME, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + PACKAGE_NAME]),
+        ("share/" + PACKAGE_NAME, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    author='Alexander Alderman Webb',
-    author_email='alexander.webb@sentry.io',
-    maintainer='Alexander Alderman Webb',
-    maintainer_email='alexander.webb@sentry.io',
-    description='A hackweek SDK for RCLPy (ROS client library for Python).',
-    license='MIT',
+    author="Alexander Alderman Webb",
+    author_email="alexander.webb@sentry.io",
+    maintainer="Alexander Alderman Webb",
+    maintainer_email="alexander.webb@sentry.io",
+    description="A hackweek SDK for RCLPy (ROS client library for Python).",
+    url="https://github.com/getsentry/sentry-rclpy",
+    license="MIT",
+    tests_require=["pytest"],
 )
